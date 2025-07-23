aee8a50865760849d6e550b4726e4b742c9745ac
martinecvia <martin.coplak@viapont.cz>
2025-07-23 10:28:51 +0200

    Created new controller for handling resources and files

---
+++ Shared/Controllers/ResourceController.cs
@@ -0,0 +1,11 @@
+namespace Shared.Controllers
+{
+    public static class ResourceController
+    {
+
+        private const string DefaultResourceIcon16Uri = "";
+        private const string DefaultResourceIcon32Uri = "";
+        private const string DefaultResourceIcon64Uri = "";
+
+    }
+}
