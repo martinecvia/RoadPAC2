7fd03717c84437b364dd9494c15312cd04ad7fbe
martinecvia <martin.coplak@viapont.cz>
2025-08-09 14:40:13 +0200

    Removed unsued imports

--- Shared/Controllers/Models/RibbonXml/Items/RibbonPanelBreak.cs
+++ Shared/Controllers/Models/RibbonXml/Items/RibbonPanelBreak.cs
@@ -1,20 +1,8 @@
-using System; // Keep for .NET 4.6
-using System.ComponentModel;
-using System.Xml.Serialization;
-
-#region O_PROGRAM_DETERMINE_CAD_PLATFORM 
-#if ZWCAD
-using ZwSoft.Windows;
-#else
-using Autodesk.Windows;
-#endif
-#endregion
-
 namespace Shared.Controllers.Models.RibbonXml.Items
 {
     // https://help.autodesk.com/view/OARX/2026/CSY/?guid=OARX-ManagedRefGuide-Autodesk_Windows_RibbonPanelBreak
     [RPPrivateUseOnly]
-    [Description("This class is used to organize a ribbon panel into main and slide-out panels. " +
+    [System.ComponentModel.Description("This class is used to organize a ribbon panel into main and slide-out panels. " +
         "Add an object of this type to RibbonPanelSource.Items to move all the items after this item into the slide-out panel. " +
         "There can be only one object of this type in RibbonPanelSource. " +
         "If there are multiple RibbonPanelBreak objects in the same panel source, only the first one will be used.")]
--- Shared/Controllers/Models/RibbonXml/Items/RibbonRowBreak.cs
+++ Shared/Controllers/Models/RibbonXml/Items/RibbonRowBreak.cs
@@ -1,20 +1,8 @@
-﻿using System; // Keep for .NET 4.6
-using System.ComponentModel;
-using System.Xml.Serialization;
-
-#region O_PROGRAM_DETERMINE_CAD_PLATFORM 
-#if ZWCAD
-using ZwSoft.Windows;
-#else
-using Autodesk.Windows;
-#endif
-#endregion
-
-namespace Shared.Controllers.Models.RibbonXml.Items
+﻿namespace Shared.Controllers.Models.RibbonXml.Items
 {
     // https://help.autodesk.com/view/OARX/2026/CSY/?guid=OARX-ManagedRefGuide-Autodesk_Windows_RibbonRowBreak
     [RPPrivateUseOnly]
-    [Description("This class is used to organize the ribbon items in a panel into multiple rows. " +
+    [System.ComponentModel.Description("This class is used to organize the ribbon items in a panel into multiple rows. " +
         "Add an object of this class to any position in a RibbonPanelSource.Items or RibbonRowPanel.Items collection to move subsequent items to the next row.")]
     public class RibbonRowBreakDef : RibbonItemDef
     { }
